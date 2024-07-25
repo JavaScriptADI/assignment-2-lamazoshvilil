e4fe783867444b32719105957028a13149a17a83
lamazoshvilil <lamazoshvilil7@gmail.com>
2024-07-25 23:28:28 +0400

    arasruli davaleba

---
+++ main.js
@@ -0,0 +1,37 @@
+// Declare a variable and assign a string as a value. Make sure the string is at least 6 characters long. Print the variable to the console.
+let firstName = "lasha" ;
+console.log (firstName) ;
+console.log (" hello " + firstName) ;
+// What is the difference between "5" and 5?
+console.log ("erti aris stringi meore kidev ricxvi")
+// Declare a variable and assign a number as a value. Make sure the number is greater than 9. Print the variable to the console.
+let age = "24";
+console.log (age);
+// Declare a variable and assign a boolean as a value. Print the variable to the console.
+let profession = "developer";
+console.log ("i wark as a" + profession);
+// What operators give us a boolean result?
+//  true && false --- (||--or && !--not &&  &&--and)
+// Declare a variable with the value of "Hello". Covert the value to upper case and print the converted value to the console
+let hi = "Hello";
+console.log ( hi.toLowerCase() + " lasha" );
+// Declare another variable with the value of "World". Convert the value to lower case and print the converted value to the console
+let earth = "WORLD";
+console.log (earth.toLowerCase());
+// What is the value of the variable text before and after the method is called? 
+// let text = 'Hello'; 
+// text.toUpperCase();
+// console.log(text);
+//  shevqmenit cvladi da mivanichet text s mniShvneloba Hello romelic iyo stringi da shemdeg am teqsts mivecit brdzaneba rom yvela aso aeyvana maGal registrshi ris merec stringad darcheba
+// 9) mnishvneloba ar migvinichebia
+// 10) toLowercase c unda iyos didi asoti C
+// 11) 
+// let ag = Number (prompt('How old are you?'));
+// let nextAge = ag + 1;
+// console.log(`Next year you will be ${nextAge}`);
+// 12)
+// let name = prompt('What is your name?');
+// let ag = Number(prompt('How old are you?'));
+// let isInSixties = ag >= 60 && ag <= 60 ;// finish the code so that it prints true if the age is in 60s
+// console.log(`${name} is in sixties, ${isInSixties}`);
+// 20 ) ar daemateba gverdigver dawers orives
